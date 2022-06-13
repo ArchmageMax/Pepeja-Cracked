3f52f33eaee5e0fd9c51f14942ed4461d7a730f8
ArchmageMax <105347091+ArchmageMax@users.noreply.github.com>
2022-06-13 20:56:09 +0800

    Photon
    photon

--- PASGIG/Assets/Scripts/ConnectToPhotonManager.cs
+++ PASGIG/Assets/Scripts/ConnectToPhotonManager.cs
@@ -9,22 +9,33 @@ using TMPro;
 public class ConnectToPhotonManager : MonoBehaviourPunCallbacks
 {   
 
-    
     public TMP_Text buttonText;
     public string username; 
 
     public void OnClickConnect() {
         Debug.Log("Clicked connect button");
         username = LoadManager.GetInstance().Username;
+        Debug.Log(username);
         if (username.Length >= 1) {
             PhotonNetwork.NickName = username;
+            Debug.Log("Nickname assigned");
             buttonText.text = "Connecting...";
+            Debug.Log("Connect Attempted");
             PhotonNetwork.ConnectUsingSettings();
+            Debug.Log("Connect Successful");
         }
-
-        
     }
-    public override void OnConnectedToMaster() {
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+        Debug.Log("Lobby Joined");
+    }
+        
+    
+    public override void OnJoinedLobby() 
+    {
         SceneManager.LoadScene("Multiplayer Lobby");
+        Debug.Log("Moved to Multiplayer");
     }
 }
